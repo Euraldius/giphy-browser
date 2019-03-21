f3da5e0135f0e7218e1acd283e4b0adde5254094
Avielle Wolfe <aviellewolfe@gmail.com>
2019-03-20 20:04:54 -0400

    Make search form accessible to screen readers
    Yay aria!
    
    https://github.com/Euraldius/giphy-browser/projects/1#card-19127950

--- src/components/Search.js
+++ src/components/Search.js
@@ -32,6 +32,7 @@ class Search extends Component {
     return (
       <form className="search" onSubmit={this.onSubmit}>
       <input
+        ariaLabel="Search for gifs"
         placeholder="SEARCH FOR GIFS"
         onChange={this.setValue}
         type="text"
